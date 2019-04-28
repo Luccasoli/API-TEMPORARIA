8e5a26c69dea32760529be13c2b22c8fc4b368c9
l <oliveira.mesquita@aluno.uece.br>
2019-04-28 05:18:31 -0300

    vai

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,26 @@
 module.exports = {
   client: "postgresql",
   connection: {
-    database: "sistema_receitas",
-    user: "postgres",
-    password: "secret"
+    database: "db9v990ido02c6",
+    user: "qbyvbqnovxmbex",
+    port: process.env.POSTGRES_PORT || 5432,
+    password: "3640c8f6fcc69c931a6694f708d8c775f3193a339171d455fabbeebe06773492"
   },
   pool: {
     min: 2,
-    max: 10
+    max: 10,
+    afterCreate: function(conn, cb) {
+      conn.query('SET timezone="UTC";', function(err) {
+        if (err) {
+          cb(err, conn);
+        } else {
+          // set trigram search match similarity limit
+          conn.query("SELECT set_limit(0.01);", function(err) {
+            cb(err, conn);
+          });
+        }
+      });
+    }
   },
   migrations: {
     tableName: "knex_migrations"
