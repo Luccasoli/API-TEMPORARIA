f8f9d0541e3aa1473b093dcd60e98c84d28239f6
Adrilene <adrilenefonseca@gmail.com>
2019-05-06 15:19:07 -0300

    desfazendo

--- src/modules/recomendado/RecomendadoRoutes.js
+++
@@ -1,7 +0,0 @@
-const routes = require("express").Router();
-const ReceitaController = require("./ReceitaController");
-
-
-routes.get("/getRecomendados/:principal", ReceitaController.getRecomendadoPrincipal);
-
-module.exports = routes;
