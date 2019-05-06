8df5afda893b5b793e30bb3e6a04642a1d680784
Adrilene <adrilenefonseca@gmail.com>
2019-05-06 15:03:07 -0300

    ajeitando erro

--- src/modules/recomendado/RecomendadoController.js
+++ src/modules/recomendado/RecomendadoController.js
@@ -3,7 +3,7 @@ const db = require("../../config/db");
 const tableName = 'receita'
 
 class RecomendadoController{
-    async getRecomendadoPrincipal(req, res) {
+    /*(async getRecomendadoPrincipal(req, res) {
         db.join("prato", "receita.autor", "=", "prato.id")
             .select("receita.nome", "prato.tipo")
             .table(tableName)
@@ -15,7 +15,17 @@ class RecomendadoController{
               .catch(err => {
                 res.status(500).json(err);
               });
-    }
+    }*/
+    async getReceitas(req, res) {
+        db.select()
+          .table(tableName)
+          .then(data => {
+            res.status(200).json(data);
+          })
+          .catch(err => {
+            res.status(500).json(err);
+          });
+      }
 }
 
 module.exports = new RecomendadoController();
